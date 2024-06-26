f8b56d2e3d435cace51a7052f3dcb4f06e673a6e
佳伟 <s2265681@163.com>
2024-06-26 16:14:34 +0800

    feat: 设置安卓的启动页

---
+++ app/src/main/java/com/kalodata/kalodata_android/SplashActivity.kt
@@ -0,0 +1,21 @@
+
+
+package com.kalodata.kalodata_android;
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.ComponentActivity
+import android.os.Handler
+import android.content.Intent
+
+class SplashActivity : ComponentActivity() {
+    private lateinit var webView: WebView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        // 设置延时跳转到主Activity，这里设置为3秒
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
