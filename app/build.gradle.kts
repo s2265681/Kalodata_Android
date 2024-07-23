fba936e58a536b637142c7307ce0d03854f57c70
佳伟 <s2265681@163.com>
2024-07-23 21:06:04 +0800

    feat: goole play

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kalodata.kalodata_android"
         minSdk = 29
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.7"
+        versionCode = 11
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
